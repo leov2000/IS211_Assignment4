2c12a494b230fad7d0cef91f8b62815c297e3914
Leonel Vergara <leovergarah@gmail.com>
2019-09-21 12:34:59 -0400

    (Added): Search examples

--- search_compare.py
+++ search_compare.py
@@ -1,11 +1,59 @@
-def sequential_search():
-    pass 
+def sequential_search(a_list, item):
+    pos = 0
+    found = False
 
-def ordered_sequential_search():
-    pass
+    while pos < len(a_list) and not found:
+        if a_list[pos] == item:
+            found = True
+        else:
+        pos = pos+1
 
-def binary_search_iterative():
-    pass 
+    return found
 
-def binary_search_recursive():
-    pass 
+def ordered_sequential_search(a_list, item):
+    pos = 0
+    found = False
+    stop = False
+
+    while pos < len(a_list) and not found and not stop:
+        if a_list[pos] == item:
+            found = True
+        else:
+            if a_list[pos] > item:
+                stop = True
+            else:
+                pos = pos+1
+                
+    return found
+
+def binary_search_iterative(a_list, item):
+    first = 0
+    last = len(a_list) - 1
+    found = False
+
+    while first <= last and not found:
+        midpoint = (first + last) // 2
+        if a_list[midpoint] == item:
+            found = True
+        else:
+            if item < a_list[midpoint]:
+                last = midpoint - 1
+        else:
+            first = midpoint + 1
+
+    return found 
+
+def binary_search_recursive(a_list, item):
+    if len(a_list) == 0:
+        return False
+    else:
+        midpoint = len(a_list) // 2
+    if a_list[midpoint] == item:
+        return True
+    else:
+        if item < a_list[midpoint]:
+            return binary_search(a_list[:midpoint], item)
+        else:
+            return binary_search(a_list[midpoint + 1:], item) 
+
+
