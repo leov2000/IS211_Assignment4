2c18088ee518f2a8dae1aa23b5c38bc5efe6897b
Leonel Vergara <leovergarah@gmail.com>
2019-09-21 12:17:45 -0400

    (Added): Sort examples

--- sort_compare.py
+++ sort_compare.py
@@ -1,8 +1,40 @@
-def insertion_sort():
-    pass 
+def insertion_sort(a_list):
+    for index in range(1, len(a_list)):
 
-def shell_sort():
-    pass 
+        current_value = a_list[index]
+        position = index
+    
+    while position > 0 and a_list[position - 1] > current_value:
+        a_list[position] = a_list[position - 1]
+        position = position - 1
 
-def python_sort();
-    pass
+    a_list[position] = current_value
+
+def shell_sort(a_list):
+    sublist_count = len(a_list)//2
+    while sublist_count > 0:
+
+        for start_position in range(sublist_count):
+            gap_insertion_sort(a_list, start_position, sublist_count)
+
+        print("After increments of size", sublist_count,
+                                    "The list is", a_list)
+
+        sublist_count = sublist_count // 2
+
+def gap_insertion_sort(a_list,start,gap):
+    for i in range(start + gap, len(a_list), gap):
+
+        current_value = a_list[i]
+        position = i
+
+        while position >= gap and a_list[position - gap] > current_value:
+            a_list[position] = a_list[position - gap]
+            position = position - gap
+
+        a_list[position] = current_value
+
+
+def python_sort(list);
+    list.sort()
+
