fc5501e1fae7ec0900bec787e693466088a3c8e1
Leonel Vergara <leovergarah@gmail.com>
2019-09-21 20:41:32 -0400

    (Added): start/end timers

--- search_compare.py
+++ search_compare.py
@@ -1,4 +1,7 @@
+import time
+
 def sequential_search(a_list, item):
+    start = time.time()
     pos = 0
     found = False
 
@@ -6,11 +9,14 @@ def sequential_search(a_list, item):
         if a_list[pos] == item:
             found = True
         else:
-        pos = pos+1
+            pos = pos+1
 
-    return found
+    end = time.time()
+
+    return (found, end- start)
 
 def ordered_sequential_search(a_list, item):
+    start = time.time()
     pos = 0
     found = False
     stop = False
@@ -23,10 +29,13 @@ def ordered_sequential_search(a_list, item):
                 stop = True
             else:
                 pos = pos+1
+    end = time.time()
                 
-    return found
+    return (found, end - start)
 
 def binary_search_iterative(a_list, item):
+    start = time.time()
+
     first = 0
     last = len(a_list) - 1
     found = False
@@ -38,22 +47,28 @@ def binary_search_iterative(a_list, item):
         else:
             if item < a_list[midpoint]:
                 last = midpoint - 1
-        else:
-            first = midpoint + 1
+            else:
+                first = midpoint + 1
+    end = time.time()            
 
-    return found 
+    return (found, end - start) 
 
-def binary_search_recursive(a_list, item):
+def binary_search_recursive(a_list, item, add_time = 0):
+    start = time.time() + add_time
     if len(a_list) == 0:
-        return False
+        end = time.time()
+        return (False, start - end)
     else:
         midpoint = len(a_list) // 2
     if a_list[midpoint] == item:
-        return True
+        end = time.time()
+        return (True, start - end)
     else:
         if item < a_list[midpoint]:
-            return binary_search(a_list[:midpoint], item)
+            end = time.time()
+            return binary_search_recursive(a_list[:midpoint], item, start)
         else:
-            return binary_search(a_list[midpoint + 1:], item) 
+            end = time.time()
+            return binary_search_recursive(a_list[midpoint + 1:], item, start) 
 
 
